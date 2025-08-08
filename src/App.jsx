8775ab93d3d12c0e69321f8318339f162d202a8e
RRRishav <rishav6787@gmail.com>
2025-08-08 11:47:23 +0530

    minor axis controls

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,28 @@ const App = () => {
 
   return (
    <>
-    <Canvas flat camera={{ fov: 35 }}>
+    <Canvas flat camera={{ fov: 32 }}>
 
       <OrbitControls />
       <ambientLight intensity={0.5} />
 
       <Scene />
      
-    
+       {/* <Bloom>
+        {/* <mesh>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
 
+       </Bloom>
+
+       <noToneMapping>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+       </noToneMapping>
+ */}
 
 
     </Canvas>
